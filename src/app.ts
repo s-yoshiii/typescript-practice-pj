30e0151b557a3af1192763135a9ffa20fdc14cf2
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-05-03 00:52:32 +0900

    シングルトン＆アプリケーションの状態管理

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
+// Project State Management
+class ProjectState {
+  private listeners: any[] = [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+  private constructor() {}
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+  addProject(title: string, description: string, manday: number) {
+    const newProject = {
+      id: Math.random().toString,
+      title: title,
+      description: description,
+      manday: manday,
+    };
+    this.projects.push(newProject);
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+const projectState = ProjectState.getInstance();
 //validate
 interface Validatable {
   value: string | number;
@@ -62,6 +92,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLElement;
   element: HTMLElement;
+  assignedProjects: any[];
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
       "project-list"
@@ -73,6 +104,9 @@ class ProjectList {
     );
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+    projectState.addListener((projects: any[]) => {
+      this.assignedProjects = projects;
+    });
     this.attach();
     this.renderContent();
   }
@@ -160,6 +194,7 @@ class ProjectInput {
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, desc, manday] = userInput;
+      projectState.addProject(title, desc, manday);
       console.log([title, desc, manday]);
       alert([title, desc, manday]);
       this.clearInputs();
